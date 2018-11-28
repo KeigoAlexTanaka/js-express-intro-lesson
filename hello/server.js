31481a6478d177e1ecf3eeec772cb32a953df08e
drake <=>
2018-11-28 09:58:11 -0500

    remove code

--- hello/server.js
+++ hello/server.js
@@ -3,13 +3,6 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.get('/:slug', (request, response) => {
-  response.send("Hello there!");
-});
-
-app.get('/name', (req, res) => {
-  res.send('drakey');
-});
 
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`);
