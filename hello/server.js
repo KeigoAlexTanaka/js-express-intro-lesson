b64f2a092a6ef3c8febad1711aa310182a5c0359
Rachel Moskowitz <rachelbmoskowitz@gmail.com>
2019-01-23 14:55:56 -0500

    added solution for parameter exercises

--- hello/server.js
+++ hello/server.js
@@ -9,7 +9,7 @@ app.listen(PORT, () => {
 });
 
 
-//Answers to first Exercise
+//Answers to Route Exercise
 app.get('/', (req, res) => {
   res.send("Welcome to my webpage!");
 })
@@ -28,4 +28,62 @@ app.get('/about-me', (req, res) => {
 
 app.get('/contact', (req, res) => {
   res.send("email: me@me.com");
+})
+
+
+//Answers to Parameters Exercise
+app.get('/students/:name', (req, res) => {
+  let epiphany = ["sandi",
+    "asheber",
+    "sam",
+    "richard",
+    "kwadwo",
+    "ashley",
+    "erika",
+    "aaron",
+    "brian",
+    "samuel",
+    "oneil",
+    "doug",
+    "claudia",
+    "aziza",
+    "heather",
+    "tessey",
+    "jason",
+    "jj",
+    "charlie",
+    "saida",
+    "danny",
+    "alex",
+    "stanly",
+    "mick",
+    "jose",
+    "christina"
+  ]
+  let name = epiphany.indexOf(req.params.name)
+
+  if (name === -1) {
+    res.send(`who is ${req.params.name}?`)
+  } else {
+    res.send(`${req.params.name} is in our class`)
+  }
+})
+
+
+app.get('/year/:year', (req, res) => {
+  if (Number(req.params.year) === NaN) {
+    res.send(`${req.params.year} is not a year`)
+  } else {
+    let year = Number(req.params.year)
+    let currentYear = 2019
+    if (year > currentYear) {
+      let diff = year - currentYear
+      res.send(`${year} is in ${diff} years`)
+    } else if (year === currentYear) {
+      res.send(`${year} is this year!`)
+    } else {
+      let diff = currentYear - year
+      res.send(`${year} is ${diff} years ago`)
+    }
+  }
 })
